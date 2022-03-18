59927be28aeffb07f17ec9bd95a416fa8643365a
bakpawel <bakpawel.poczta@gmail.com>
2022-03-18 21:09:15 +0100

    added interface configuration and empty service

---
+++ src/app/weather.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  //for fetching data from server
+  constructor() {}
+}
---
+++ src/app/weather.ts
@@ -0,0 +1,22 @@
+export interface Weather {
+  weather: Info[];
+  main: {
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+  };
+  sys: {
+    country: string;
+  };
+  name: string;
+}
+
+interface Info {
+  description: string;
+  icon: string;
+}
